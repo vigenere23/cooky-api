9680c129730e275d45e808b3fa5a8e1d6b9ec6ed
vigenere23 <lolgab1@hotmail.com>
2019-03-19 13:00:04 -0400

    added echos to scripts + added windows version (to run with bash)

--- scripts/init-database.sh
+++ scripts/init-database.sh
@@ -1,4 +1,7 @@
+echo "Starting database initiation scripts"
+echo "This might take a while..."
 docker exec -i mysql1 mysql < bd_init/database.sql
 docker exec -i mysql1 mysql < bd_init/tables.sql
 # docker exec -i mysql1 mysql < bd_init/triggers.sql
 docker exec -i mysql1 mysql < bd_init/inserts.sql
+echo "Done!"
--- scripts/win-start-docker.sh
+++ scripts/win-start-docker.sh
@@ -3,7 +3,6 @@ docker stop mysql1
 docker run -d \
 --name mysql1 --rm \
 -p 1337:3306 \
---volume $PWD/mysql:/docker-entrypoint-initdb.d/:ro \
 --env "MYSQL_ALLOW_EMPTY_PASSWORD=yes" mysql:8.0
 docker start mysql1
 echo "Done!"
