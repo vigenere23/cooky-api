513c99c7c9ae86135f52129a48504d7250bd7996
vigenere23 <lolgab1@hotmail.com>
2019-03-31 11:46:38 -0400

    removed material design icons module -> replaced with html link

--- ui/src/main.js
+++ ui/src/main.js
@@ -5,7 +5,6 @@ import store from './js/store'
 
 require('typeface-roboto')
 require('typeface-merriweather')
-require('material-design-icons/iconfont/material-icons.css')
 
 Vue.config.productionTip = false
 
