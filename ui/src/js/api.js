c041274057cc4e5e14c90b394fce48adfc8a9a60
vigenere23 <lolgab1@hotmail.com>
2019-04-06 18:09:47 -0400

    small cleaning

---
+++ ui/src/js/api.js
@@ -0,0 +1,82 @@
+import { AxiosHelper } from '@/js/helpers/axios'
+const BASE_URL = ' http://127.0.0.1:5000'
+
+export class API {
+
+  static async getUserById() {
+
+  }
+
+  static async getRecipeByUser() {
+
+  }
+
+  static async getLikedRecipes() {
+
+  }
+
+  static async getProfile() {
+
+  }
+
+  static async getAllCart() {
+
+  }
+
+  static async getRecipe() {
+
+  }
+
+  static async getRecipeById() {
+
+  }
+
+  static async getIngredientFromIdRecipe() {
+
+  }
+
+  static async getIngredients() {
+
+  }
+
+  static async getIngredientByName() {
+
+  }
+
+  static async getCartItem() {
+
+  }
+
+  static async getCommandFromCart() {
+
+  }
+
+  static async addIngredientToCart() {
+
+  }
+
+  static async addRecipe() {
+
+  }
+
+  static async addComment() {
+
+  }
+
+  static async addLike() {
+
+  }
+
+  static async addRating() {
+
+  }
+
+  static async addNewCommand() {
+
+  }
+
+  static async addNewUser() {
+
+  }
+
+}
