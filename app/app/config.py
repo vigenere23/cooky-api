494403723a06b5884c8a6180cca4cad655ba452d
vigenere23 <lolgab1@hotmail.com>
2021-06-21 10:50:12 -0400

    added taskfile + .env file

--- app/app/config.py
+++ app/app/config.py
@@ -1,14 +1,14 @@
 import os
 
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY', '4729ef76-3d15-4356-8c0c-a54137c1250a')
+  SECRET_KEY = os.environ.get('SECRET_KEY')
   JWT_AUTH_URL_RULE = '/login/'
   DATABASE = {
-    'user': os.environ.get('MYSQL_USER', 'api'),
-    'password': os.environ.get('MYSQL_PASSWORD', 'e7364202-2f94-4355-b354-c95907adfef6'),
-    'host': os.environ.get('DB_HOST', 'db'),
-    'port': os.environ.get('DB_PORT', '3306'),
-    'database': os.environ.get('MYSQL_DATABASE', 'projet'),
+    'user': 'api',
+    'password': os.environ.get('MYSQL_PASSWORD'),
+    'host': os.environ.get('DB_HOST'),
+    'port': os.environ.get('DB_PORT'),
+    'database': 'projet',
     'connection_timeout': 5,
     'use_pure': True
   }
