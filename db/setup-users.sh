6a20b3e7539f81079b1c29f0cbbd362a8d7f3c59
vigenere23 <lolgab1@hotmail.com>
2021-08-16 08:25:26 -0400

    Removed Provider user + fixed docker image

--- db/setup-users.sh
+++ db/setup-users.sh
@@ -1,6 +1,5 @@
-# cp ./init/privileges.sql /docker-entrypoint-initdb.d
+cp ./init/privileges.sql /docker-entrypoint-initdb.d
 
 filename=/docker-entrypoint-initdb.d/0_users.sql
 echo "" > $filename
 echo "CREATE USER 'api'@'%' IDENTIFIED BY '$MYSQL_API_PASSWORD';" >> $filename
-echo "CREATE USER 'provider'@'%' IDENTIFIED BY '$MYSQL_PROVIDER_PASSWORD';" >> $filename
